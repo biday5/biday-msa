aadef333e7a7e368ad82c5e42dc876e0da9a1eb6
kimdohee58 <kimdohee58@gmail.com>
2024-10-18 19:25:41 +0900

    feat : product-service findBySize

---
+++ Desktop/biday-msa/backend/buildModule.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 나중엔 서버 기준으로 변경할 것
+export JAVA_HOME="C:\\Program Files\\Java\\jdk-17"
+
+chmod +x ./gradlew
+
+# 모듈 리스트
+all_modules=(
+    "server:config-server"
+    "server:eureka-server"    
+    "server:gateway-server"
+    "service:admin-service"
+    "service:auction-service"
+    "service:ftp-service"
+    "service:order-service"
+    "service:product-service"
+    "service:sms-service"
+    "service:user-service"
+)
+
+# Gradle clean
+echo "Cleaning..."
+./gradlew clean
+
+# Gradle BootJar
+for module in "${all_modules[@]}"
+do
+    echo "Building for $module"
+    ./gradlew :$module:build
+done
