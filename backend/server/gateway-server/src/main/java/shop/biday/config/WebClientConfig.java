50f68b89a3faa9ebf3e8b2288faed15b2640c9e8
kimdohee58 <kimdohee58@gmail.com>
2024-10-26 18:50:33 +0900

    fix: webClient

--- backend/server/gateway-server/src/main/java/shop/biday/config/WebClientConfig.java
+++ backend/server/gateway-server/src/main/java/shop/biday/config/WebClientConfig.java
@@ -2,16 +2,24 @@ package shop.biday.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
+
     @Bean
     public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(5)); // 응답 타임아웃 설정
+
         return WebClient.builder()
-                .baseUrl("http://110.165.19.104:8000") // 기본 URL 설정 (필요에 따라 변경)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .baseUrl("http://110.165.19.104:8000")
                 .build();
     }
-
 }
