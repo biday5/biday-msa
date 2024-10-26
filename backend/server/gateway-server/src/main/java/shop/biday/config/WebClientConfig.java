f6b84bc8234798d3cfde0ff4c1c5a394b7b84b3e
kimdohee58 <kimdohee58@gmail.com>
2024-10-26 21:08:10 +0900

    fix : defaultHeader

--- backend/server/gateway-server/src/main/java/shop/biday/config/WebClientConfig.java
+++ backend/server/gateway-server/src/main/java/shop/biday/config/WebClientConfig.java
@@ -2,9 +2,11 @@ package shop.biday.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import org.springframework.http.HttpHeaders;
 
 import java.time.Duration;
 
@@ -20,6 +22,7 @@ public class WebClientConfig {
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl("http://110.165.19.104:8000")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
