1bd1ed179ab2ed587147bcf9ca0e203e30187115
kimdohee58 <kimdohee58@gmail.com>
2024-10-27 17:25:06 +0900

    fix : log

--- backend/server/gateway-server/src/main/java/shop/biday/config/WebClientConfig.java
+++ backend/server/gateway-server/src/main/java/shop/biday/config/WebClientConfig.java
@@ -1,17 +1,21 @@
 package shop.biday.config;
 
 import io.netty.handler.logging.LogLevel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 import java.time.Duration;
 
+@Slf4j
 @Configuration
 public class WebClientConfig {
 
@@ -26,6 +30,14 @@ public class WebClientConfig {
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl("http://110.165.19.104:8000")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .filter(logRequest()) // 요청 로깅 필터 추가
                 .build();
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            log.info("Requesting URI: {}", clientRequest.url());
+            return Mono.just(clientRequest);
+        });
     }
 }
