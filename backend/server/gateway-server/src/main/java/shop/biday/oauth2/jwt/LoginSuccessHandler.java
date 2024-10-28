81f7c922a01761bba57a26f0eadaead4d326bb9b
hwi <gnltps18@naver.com>
2024-10-28 15:14:35 +0900

    refactor : 내려주는 토큰 .secure(false) /   .sameSite(Lax) 삭제

--- backend/server/gateway-server/src/main/java/shop/biday/oauth2/jwt/LoginSuccessHandler.java
+++ backend/server/gateway-server/src/main/java/shop/biday/oauth2/jwt/LoginSuccessHandler.java
@@ -98,8 +98,7 @@ public class LoginSuccessHandler {
         return ResponseCookie.from(key, value)
                 .maxAge(COOKIE_MAX_AGE_SECONDS)
                 .path("/")
-                .secure(false)  // HTTPS에서만 전송
-                .sameSite("Lax") // SameSite=Lax 설정
+                .httpOnly(false)
                 .build();
     }
 
--- backend/server/gateway-server/src/main/java/shop/biday/oauth2/jwt/Oauth2SuccessHandler.java
+++ backend/server/gateway-server/src/main/java/shop/biday/oauth2/jwt/Oauth2SuccessHandler.java
@@ -103,8 +103,6 @@ public class Oauth2SuccessHandler implements ServerAuthenticationSuccessHandler 
                 .maxAge(COOKIE_MAX_AGE_SECONDS)
                 .path("/")
                 .httpOnly(false)
-                .secure(false)  // HTTPS에서만 전송
-                .sameSite("Lax") // SameSite=Lax 설정
                 .build();
     }
 
