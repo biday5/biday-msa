e7976498ebedbb7890c354958944966484b43e75
hwi <gnltps18@naver.com>
2024-10-28 21:21:52 +0900

    feat : 입찰내역조회  (새로운 기능)

--- backend/service/auction-service/src/main/java/shop/biday/service/BidService.java
+++ backend/service/auction-service/src/main/java/shop/biday/service/BidService.java
@@ -1,11 +1,15 @@
 package shop.biday.service;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import shop.biday.model.document.BidDocument;
 import shop.biday.model.domain.BidModel;
+import shop.biday.model.dto.BidDto;
 import shop.biday.model.dto.BidResponse;
 
 public interface BidService {
+
+    Flux<BidDto> findByUserId(String userInfo);
 
     Mono<BidResponse> save(String userInfo, BidModel bid);
 
