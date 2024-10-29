39d23533511254aac08a069f3eb82c8a90153da0
kimdohee58 <kimdohee58@gmail.com>
2024-10-29 16:35:13 +0900

    fix : order-service payment findByUser ì정

---
+++ backend/service/order-service/src/main/java/shop/biday/model/dto/PaymentData.java
@@ -0,0 +1,26 @@
+package shop.biday.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentData {
+    private Long awardId;
+    private String paymentKey;
+    private Long amount;
+    private String orderId;
+    private LocalDateTime approvedAt;
+}
--- backend/service/order-service/src/main/java/shop/biday/model/repository/QPaymentRepository.java
+++ backend/service/order-service/src/main/java/shop/biday/model/repository/QPaymentRepository.java
@@ -1,10 +1,11 @@
 package shop.biday.model.repository;
 
+import shop.biday.model.dto.PaymentData;
 import shop.biday.model.dto.PaymentRequest;
 
 import java.util.List;
 
 public interface QPaymentRepository {
 
-    List<PaymentRequest> findByUser(String userId);
+    List<PaymentData> findByUser(String userId);
 }
